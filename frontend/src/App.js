f3af102e791c3d688c080833757bf836b55a0563
Rogerio Gracini <rogracini@gmail.com>
2025-02-26 09:40:15 -0300

    Primeiro commit

---
+++ frontend/src/App.js
@@ -0,0 +1,22 @@
+// frontend/src/App.js
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Trabalho de Pipeline DevSecOps</h1>
+      <p>Bem-vindo à aplicação Pipeline DevSecOps</p>
+    </div>
+  );
+}
+
+export default App;
+
+// frontend/Dockerfile
+FROM node:16
+WORKDIR /app
+COPY package*.json ./
+RUN npm install
+COPY . .
+EXPOSE 3000
+CMD ["npm", "start"]
